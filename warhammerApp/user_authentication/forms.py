367855e2fcd37aaac95d6640d21a2df8ca3bc422
Chlodwig-tech <szczepan.chojnowski@tutanota.com>
2023-01-11 14:45:03 +0100

    create login_view

--- warhammerApp/user_authentication/forms.py
+++ warhammerApp/user_authentication/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-class  NewUserForm(UserCreationForm):
+class NewUserForm(UserCreationForm):
     username = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', "placeholder": "Username"}))
     email    = forms.EmailField(label='', widget=forms.TextInput(attrs={'class': 'form-control', "placeholder": "Email"}))
     password1 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
@@ -23,3 +23,7 @@ class  NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', "placeholder": "Username"}))
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
--- warhammerApp/user_authentication/views.py
+++ warhammerApp/user_authentication/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, redirect
-from .forms import NewUserForm
-from django.contrib.auth import login
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm, LoginForm
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+
+from utilities.authenticators import when_logged_in, when_logged_out
+
 # Create your views here.
 
+@when_logged_out
 def register_view(request, *args, **kwargs):
     if request.method == 'POST':
         form = NewUserForm(request.POST)
@@ -13,7 +17,7 @@ def register_view(request, *args, **kwargs):
             user = form.save()
             login(request, user)
             messages.success(request, 'Registration successful')
-            return redirect('/auth/login')
+            return redirect('/login')
 
         messages.error(request, 'Unsuccessful registration. Invalid information')
 
@@ -22,3 +26,33 @@ def register_view(request, *args, **kwargs):
         'register_form' : form
     }
     return render(request, 'register.html', context)
+
+@when_logged_out
+def login_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = LoginForm(request.POST, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('/welcome')
+            messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+            
+    form = LoginForm()
+    context = {
+        'login_form' : form
+    }
+    return render(request, 'login.html', context)
+
+@when_logged_in
+def logout_view(request, *args, **kwargs):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect('/welcome')
